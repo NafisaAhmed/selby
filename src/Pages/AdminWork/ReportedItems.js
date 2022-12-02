59cbe9f95d9b947edf3fb71a94995bd514225d87
Nafisa Ahmed <ahmednafisa11@gmail.com>
2022-12-02 12:14:31 +0600

    reported items display and delete from admin done

--- src/Pages/AdminWork/ReportedItems.js
+++ src/Pages/AdminWork/ReportedItems.js
@@ -1,9 +1,64 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const ReportedItems = () => {
+    let { data: reports = [], refetch } = useQuery({
+        queryKey: ['reportedproducts'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/reportedproducts');
+            const data = await res.json();
+            return data;
+        }
+    })
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/reportedproducts/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    toast.success('Report Deleted Succesfully');
+                    refetch();
+                }
+            })
+    }
     return (
         <div>
-            <h2>report</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Product</th>
+                            <th>Buyer Name</th>
+                            <th>Buyer Email</th>
+                            <th>Seller Name</th>
+                            <th>Report</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            reports.map((report) => <tr key={report._id}>
+
+                                <th>
+                                    <label>
+                                        <button onClick={() =>
+                                            handleDelete(report._id)} className="btn btn-ghost btn-xs">X</button>
+                                    </label>
+                                </th>
+                                <td>{report.product_name}</td>
+                                <td>{report.buyer}</td>
+                                <td>{report.email}</td>
+                                <td>{report.seller}</td>
+                                <td>{report.report}</td>
+                            </tr>)
+
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
